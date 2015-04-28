500df4e7dfbedf1eed826d2fa0c25bb12ce19afe
jessehuze <jbhf39@mst.edu>
2015-04-28 18:25:40 -0500

    Removed all of the app shit

--- login/login.js
+++ login/login.js
@@ -31,23 +31,4 @@ $(document).ready(function() {
     $(".login").css("display", "block");
   });
   
-  $(document).on("click", ".app__logout", function(e) {
-    if (animating) return;
-    $(".ripple").remove();
-    animating = true;
-    var that = this;
-    $(that).addClass("clicked");
-    setTimeout(function() {
-      $app.removeClass("active");
-      $login.show();
-      $login.css("top");
-      $login.removeClass("inactive");
-    }, logoutPhase1 - 120);
-    setTimeout(function() {
-      $app.hide();
-      animating = false;
-      $(that).removeClass("clicked");
-    }, logoutPhase1);
-  });
-  
 });
