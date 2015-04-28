5d52b43fd1b52bc5034e3b4d0f9aee13f36d96e7
jessehuze <jbhf39@mst.edu>
2015-04-28 17:33:00 -0500

    Testing the redirect

--- login/processlogin.php
+++ login/processlogin.php
@@ -2,7 +2,7 @@
   require("/var/www/config.php");
   if($_POST["username"] != "" && $_POST["pass"] != "" && isset($_POST["login"])) {
     $result = mysqli_query($con, "SELECT username, usr_pass FROM PEOPLE WHERE username = '" .$_POST["username"]. "'");
-    if (!$result) {
+    if (true) {
       $_SESSION["loginerror"] = 1;
       header("Location: http://inceptisol.us.to:6670/login/login.php");
       exit();
