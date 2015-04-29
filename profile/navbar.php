367a69ae3e2e8f697d84f3c47b8604a067672594
jessehuze <jbhf39@mst.edu>
2015-04-28 23:42:15 -0500

    Added dropdown bar for prof

--- profile/navbar.php
+++ profile/navbar.php
@@ -19,6 +19,14 @@
         <li><a href="friends.php">Friends</a></li>
         <li><a href="groups.php">Groups</a></li>
         <li><a href="profile.php">Profile</a></li>
+        <li class="dropdown">
+          <a href="#" >Profile<span class="dropdown-toggle caret" data-toggle="dropdown" role="button" aria-expanded="false"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Profile</a></li>
+            <li><a href="#">Settings</a></li>
+            <li><a href="#">Log Out</a></li>
+          </ul>
+        </li>
         <li><a href="settings.php">Settings</a></li>
         <li><a href="../logout.php"><i class="fa fa-power-off"></i> Log Out</a></li>
       </ul>
