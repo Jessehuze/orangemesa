55f7a4dc0640264b4090ac38ebfdf94f135c8ff4
Somsomguy <someguywhohasaname@gmail.com>
2015-04-29 15:44:40 -0500

    Adjustments to groups

--- profile/createGroup.php
+++ profile/createGroup.php
@@ -1,11 +1,16 @@
 <?php
 	require("/var/www/config.php");
 	session_start();
-	$result = mysqli_query($con, "SELECT groupname FROM GROUPS WHERE groupname = '" .$POST["groupName"]. "'");
+	$result = mysqli_query($con, "SELECT name FROM GROUPS WHERE name = '" .$POST["groupName"]. "'");
 	if(mysqli_num_rows($result) == 0)
 	{
-		$insert = mysqli_query($con, "INSERT INTO GROUPS (groupname, description)
-									VALUES ('".$POST["groupName"]."', '".$POST["groupDesc"]."')");
+		$insert = mysqli_query($con, "INSERT INTO GROUPS (name, description, owner)
+									VALUES ('".$POST["groupName"]."', '".$POST["groupDesc"]."', '".SESSION["User"]."')");
+	}
+	else
+	{
+		header("Location: http://inceptisol.us.to:6670/profile/groups.php?error=groupnametaken");
+		exit();
 	}
 	header("Location: http://inceptisol.us.to:6670/profile/groups.php");
 	exit();
