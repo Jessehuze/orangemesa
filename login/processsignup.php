67b03eedc335adc7f930523367ad08d5a0f2ccb3
jessehuze <jbhf39@mst.edu>
2015-04-28 20:40:56 -0500

    Changed empty validation

--- login/processsignup.php
+++ login/processsignup.php
@@ -1,13 +1,13 @@
 <?php
   require("/var/www/config.php");
   $result = mysqli_query($con, "SELECT username FROM PEOPLE WHERE username = '" .$_POST["username"]. "'");
-  if (empty($result) && $_POST["mname"] == "") { //NO MIDDLE NAME GIVEN
+  if (mysql_num_rows($result) == 0 && $_POST["mname"] == "") { //NO MIDDLE NAME GIVEN
     
     $insert = mysqli_query($con, "INSERT INTO PEOPLE (username, usr_pass, fname, lname, dob)
     									VALUES ('".$_POST["username"]."', '".$_POST["usr_pass"]."', 
                                     			'".$_POST["fname"]."', '".$_POST["lname"]."', ".$_POST["dob"]."");
   }
-  else if (empty($result)) { //MIDDLE NAME GIVEN
+  else if (mysql_num_rows($result) == 0) { //MIDDLE NAME GIVEN
     $insert = mysqli_query($con, "INSERT INTO PEOPLE (username, usr_pass, fname, minit, lname, dob)
     									VALUES ('".$_POST["username"]."', '".$_POST["usr_pass"]."', '".$_POST["minit"]."',
                                     			'".$_POST["fname"]."', '".$_POST["lname"]."' , ".$_POST["dob"]."");
