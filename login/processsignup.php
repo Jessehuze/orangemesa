180939b43b56c55547025eaad7b31bbde56b2881
jessehuze <jbhf39@mst.edu>
2015-04-28 20:50:02 -0500

    just don't

--- login/processsignup.php
+++ login/processsignup.php
@@ -2,13 +2,13 @@
   require("/var/www/config.php");
   $result = mysqli_query($con, "SELECT username FROM PEOPLE WHERE username = '" .$_POST["username"]. "'");
   echo $result;
-  if (mysql_num_rows($result) == 0 && $_POST["minit"] == "") { //NO MIDDLE NAME GIVEN
+  if (mysqli_num_rows($result) == 0 && $_POST["minit"] == "") { //NO MIDDLE NAME GIVEN
     
     $insert = mysqli_query($con, "INSERT INTO PEOPLE (username, usr_pass, fname, lname, dob)
     									VALUES ('".$_POST["username"]."', '".$_POST["usr_pass"]."', 
                                     			'".$_POST["fname"]."', '".$_POST["lname"]."', '".$_POST["dob"]."'");
   }
-  else if (mysql_num_rows($result) == 0) { //MIDDLE NAME GIVEN
+  else if (mysqli_num_rows($result) == 0) { //MIDDLE NAME GIVEN
     $insert = mysqli_query($con, "INSERT INTO PEOPLE (username, usr_pass, fname, minit, lname, dob)
     									VALUES ('".$_POST["username"]."', '".$_POST["usr_pass"]."', '".$_POST["minit"]."',
                                     			'".$_POST["fname"]."', '".$_POST["lname"]."' , '".$_POST["dob"]."'");
