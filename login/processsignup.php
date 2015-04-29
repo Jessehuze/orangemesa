4f48d6c519eed55146c3c2e657654cbadef25458
jessehuze <jbhf39@mst.edu>
2015-04-28 20:57:51 -0500

    fukking parathereareasdfkdajlseees

--- login/processsignup.php
+++ login/processsignup.php
@@ -5,16 +5,13 @@
     
     $insert = mysqli_query($con, "INSERT INTO PEOPLE (username, usr_pass, fname, lname, dob)
     									VALUES ('".$_POST["username"]."', '".$_POST["usr_pass"]."', 
-                                    			'".$_POST["fname"]."', '".$_POST["lname"]."', '".$_POST["dob"]."'");
-    echo "INSERT INTO PEOPLE (username, usr_pass, fname, lname, dob)
-    									VALUES ('".$_POST["username"]."', '".$_POST["usr_pass"]."', 
-                                    			'".$_POST["fname"]."', '".$_POST["lname"]."', '".$_POST["dob"]."'";
+                                    			'".$_POST["fname"]."', '".$_POST["lname"]."', '".$_POST["dob"]."')");
     echo mysqli_error($con);
   }
   else if (mysqli_num_rows($result) == 0) { //MIDDLE NAME GIVEN
     $insert = mysqli_query($con, "INSERT INTO PEOPLE (username, usr_pass, fname, minit, lname, dob)
     									VALUES ('".$_POST["username"]."', '".$_POST["usr_pass"]."', '".$_POST["minit"]."',
-                                    			'".$_POST["fname"]."', '".$_POST["lname"]."' , '".$_POST["dob"]."'");
+                                    			'".$_POST["fname"]."', '".$_POST["lname"]."' , '".$_POST["dob"]."')");
 
   }
   else //Redirect otherwise
