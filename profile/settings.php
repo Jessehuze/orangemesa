38c894d0b20e9670a51e980cda292810dbb7ea16
nrhvyc <nrhvyc.nickryanhughes@gmail.com>
2015-04-29 16:22:03 -0500

    Working on Settings

--- profile/settings.php
+++ profile/settings.php
@@ -76,12 +76,12 @@
 
        <form action="processEdit_minit.php" method="POST">
         <input name="minit" type="text" class="edit__input minit" placeholder="Middle Initial" required/>
-        <button name="update_fname" type="submit" class="edit__submit_fname">Update Middle Initial</button>
+        <button name="update_minit" type="submit" class="edit__submit_minit">Update Middle Initial</button>
        </div>
 
        <form action="processEdit_lname.php" method="POST">
         <input name="lname" type="text" class="edit__input lname" placeholder="Last Name" required/>
-        <button name="update_fname" type="submit" class="edit__submit_fname">Update Last Name</button>
+        <button name="update_lname" type="submit" class="edit__submit_lname">Update Last Name</button>
        </div>
       </div>
      </div>
