09c6f5a3574258021ce38bb2fd028bbb31770017
jessehuze <jbhf39@mst.edu>
2015-04-28 21:05:52 -0500

    Added login validation

--- login/processlogin.php
+++ login/processlogin.php
@@ -1,8 +1,9 @@
 <?php
   require("/var/www/config.php");
   if($_POST["username"] != "" && $_POST["pass"] != "" && isset($_POST["login"])) {
-    $result = mysqli_query($con, "SELECT username, usr_pass FROM PEOPLE WHERE username = '" .$_POST["username"]. "'");
-    if (true) {
+    $result = mysqli_query($con, "SELECT username, usr_pass FROM PEOPLE WHERE username = '" .$_POST["username"]. "' AND
+                                                                              usr_pass = '" .$_POST["pass"]."'");
+    if (mysqli_num_rows($result) == 0) {
       $_SESSION["loginerror"] = 1;
       header("Location: http://inceptisol.us.to:6670/login/login.php");
       exit();
@@ -10,5 +11,7 @@
     $row = mysqli_fetch_array($result);
     echo $row["username"]; 
     $_SESSION["User"] = $row["username"];
+    header("Location: http://inceptisol.us.to:6670/template/template.php");
+    exit();
   }
 ?>
