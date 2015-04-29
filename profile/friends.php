8e8a0965faecf48d1356c69c276b1b666f62caf5
Jesse Hughes <jbhf39@mst.edu>
2015-04-29 10:28:14 -0500

    Removed navbar.php and reintroduced it into each individual page. This allowed for the use of class=active

--- profile/friends.php
+++ profile/friends.php
@@ -31,9 +31,36 @@
   
   <body>
 
-    <?php 
-      require("navbar.php");
-    ?>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <img href="../images/logo.png"></img>
+          <a class="navbar-brand" href="profile.php">
+            <object id="logoO" type="image/svg+xml" data="../images/logo.svg" width="25" height="25"></object>
+            <object id="logoW" type="image/svg+xml" data="../images/logow.svg" width="25" height="25" style="position: relative; left: -30.25px; opacity: 0;"></object>
+            <span style="position: relative; top: -5.5px; left: -37.25px;">rangeMesa</span></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="events.php">Events</a></li>
+            <li class="active"><a href="friends.php">Friends</a></li>
+            <li><a href="groups.php">Groups</a></li>
+            <li><a href="profile.php">Profile</a></li>
+            <li><a href="settings.php">Settings</a></li>
+            <li><a href="../logout.php"><i class="fa fa-power-off"></i> Log Out</a></li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <input type="text" class="form-control" placeholder="Search...">
+          </form>
+        </div>
+      </div>
+    </nav>
 
     <div class="container-fluid">
       <div class="row">
