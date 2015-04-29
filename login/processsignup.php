3bbad4ea88cce5a8fb1bea29871a5b40a52e98b7
jessehuze <jbhf39@mst.edu>
2015-04-28 20:54:12 -0500

    fuking:

--- login/processsignup.php
+++ login/processsignup.php
@@ -6,11 +6,13 @@
     $insert = mysqli_query($con, "INSERT INTO PEOPLE (username, usr_pass, fname, lname, dob)
     									VALUES ('".$_POST["username"]."', '".$_POST["usr_pass"]."', 
                                     			'".$_POST["fname"]."', '".$_POST["lname"]."', '".$_POST["dob"]."'");
+    echo mysqli_error($con);
   }
   else if (mysqli_num_rows($result) == 0) { //MIDDLE NAME GIVEN
     $insert = mysqli_query($con, "INSERT INTO PEOPLE (username, usr_pass, fname, minit, lname, dob)
     									VALUES ('".$_POST["username"]."', '".$_POST["usr_pass"]."', '".$_POST["minit"]."',
                                     			'".$_POST["fname"]."', '".$_POST["lname"]."' , '".$_POST["dob"]."'");
+
   }
   else //Redirect otherwise
   {
