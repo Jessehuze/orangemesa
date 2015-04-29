e2c20a25e3b4b585de110a6992f86b5f457a4568
jessehuze <jbhf39@mst.edu>
2015-04-28 20:42:31 -0500

    Fixed minit

--- login/processsignup.php
+++ login/processsignup.php
@@ -1,16 +1,16 @@
 <?php
   require("/var/www/config.php");
   $result = mysqli_query($con, "SELECT username FROM PEOPLE WHERE username = '" .$_POST["username"]. "'");
-  if (mysql_num_rows($result) == 0 && $_POST["mname"] == "") { //NO MIDDLE NAME GIVEN
+  if (mysql_num_rows($result) == 0 && $_POST["minit"] == "") { //NO MIDDLE NAME GIVEN
     
     $insert = mysqli_query($con, "INSERT INTO PEOPLE (username, usr_pass, fname, lname, dob)
     									VALUES ('".$_POST["username"]."', '".$_POST["usr_pass"]."', 
-                                    			'".$_POST["fname"]."', '".$_POST["lname"]."', ".$_POST["dob"]."");
+                                    			'".$_POST["fname"]."', '".$_POST["lname"]."', '".$_POST["dob"]."'");
   }
   else if (mysql_num_rows($result) == 0) { //MIDDLE NAME GIVEN
     $insert = mysqli_query($con, "INSERT INTO PEOPLE (username, usr_pass, fname, minit, lname, dob)
     									VALUES ('".$_POST["username"]."', '".$_POST["usr_pass"]."', '".$_POST["minit"]."',
-                                    			'".$_POST["fname"]."', '".$_POST["lname"]."' , ".$_POST["dob"]."");
+                                    			'".$_POST["fname"]."', '".$_POST["lname"]."' , '".$_POST["dob"]."'");
   }
   else //Redirect otherwise
   {
