219569c44ca78fa391bf872b2c505486ac3bd94e
jessehuze <jbhf39@mst.edu>
2015-04-28 20:56:27 -0500

    our guy

--- login/processsignup.php
+++ login/processsignup.php
@@ -6,6 +6,9 @@
     $insert = mysqli_query($con, "INSERT INTO PEOPLE (username, usr_pass, fname, lname, dob)
     									VALUES ('".$_POST["username"]."', '".$_POST["usr_pass"]."', 
                                     			'".$_POST["fname"]."', '".$_POST["lname"]."', '".$_POST["dob"]."'");
+    echo "INSERT INTO PEOPLE (username, usr_pass, fname, lname, dob)
+    									VALUES ('".$_POST["username"]."', '".$_POST["usr_pass"]."', 
+                                    			'".$_POST["fname"]."', '".$_POST["lname"]."', '".$_POST["dob"]."'";
     echo mysqli_error($con);
   }
   else if (mysqli_num_rows($result) == 0) { //MIDDLE NAME GIVEN
