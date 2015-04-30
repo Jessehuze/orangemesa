478e9cca0aa1ccf4a01046c6b5c1361a5a72f85a
Somsomguy <someguywhohasaname@gmail.com>
2015-04-29 23:27:24 -0500

    Groups kindof works

--- profile/createGroup.php
+++ profile/createGroup.php
@@ -5,13 +5,14 @@
 	$result = mysqli_query($con, $query);
 	if(mysqli_num_rows($result) == 0)
 	{
+		//Create Group in database
 		$query = "INSERT INTO GROUPS (name, description, owner) VALUES ('".$_POST["groupName"]."', '".$_POST["groupDesc"]."', '".$_SESSION["username"]."')";
 		$insert = mysqli_query($con, $query);
 		
 		$query = mysqli_query($con, "SELECT groupid, owner FROM GROUPS WHERE name = '".$_POST["groupName"]."'");
 		$row1 =mysqli_fetch_array($query);
 		
-		//Insert in owner as member of group
+		//Insert owner as member of group
 		$query = "INSERT INTO GROUP_MEMBERS (GID, memberid) VALUES('".$row1["groupid"]."','".$row1["owner"]."')";
 		$insert = mysqli_query($con, $query);
 	}
