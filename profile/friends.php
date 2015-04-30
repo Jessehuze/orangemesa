07475cd18ea1d950a9788a6769961a247c7b084f
nrhvyc <nrhvyc.nickryanhughes@gmail.com>
2015-04-29 22:41:37 -0500

    Working on Settings

--- profile/friends.php
+++ profile/friends.php
@@ -49,7 +49,7 @@
 
                <div class="editProfile_row">
                 <form action="updateProfile/processEdit_fname.php" method="POST">
-                <input name="refer" type="hidden" value="/profile/friends.php"/>
+                <input name="refer" type="hidden" value="http://inceptisol.us.to:6670//profile/friends.php"/>
                 <input name="fname" type="text" class="edit__input fname" placeholder="First Name" />
                 <button name="update_fname" type="submit" class="edit__submit">Update First Name</button>
                 </form>
@@ -57,7 +57,7 @@
 
                <div class="editProfile_row">
                 <form action="updateProfile/processEdit_minit.php" method="POST">
-                <input name="refer" type="hidden" value="/profile/friends.php"/>
+                <input name="refer" type="hidden" value="http://inceptisol.us.to:6670//profile/friends.php"/>
                 <input name="minit" type="text" class="edit__input minit" placeholder="Middle Initial" />
                 <button name="update_minit" type="submit" class="edit__submit">Update Middle Initial</button>
                 </form>
@@ -65,7 +65,7 @@
 
                <div class="editProfile_row">
                 <form action="updateProfile/processEdit_lname.php" method="POST">
-                <input name="refer" type="hidden" value="/profile/friends.php"/>
+                <input name="refer" type="hidden" value="http://inceptisol.us.to:6670//profile/friends.php"/>
                 <input name="lname" type="text" class="edit__input lname" placeholder="Last Name" />
                 <button name="update_lname" type="submit" class="edit__submit">Update Last Name</button>
                 </form>
@@ -73,7 +73,7 @@
 
                <div class="editProfile_row">
                 <form action="updateProfile/processEdit_description.php" method="POST">
-                <input name="refer" type="hidden" value="/profile/friends.php"/>
+                <input name="refer" type="hidden" value="http://inceptisol.us.to:6670//profile/friends.php"/>
                 <textarea name="description" class="edit__input description" placeholder="Description" rows="6"></textarea>
                 <button name="update_description" type="submit" class="edit__submit">Update Description</button>
                 </form>
