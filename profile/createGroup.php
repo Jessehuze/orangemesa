9d24b54dfd675ed5bf6384824107f863b3466e19
Somsomguy <someguywhohasaname@gmail.com>
2015-04-29 20:17:02 -0500

    debugging

--- profile/createGroup.php
+++ profile/createGroup.php
@@ -3,6 +3,7 @@
 	session_start();
 	$query = "SELECT name FROM GROUPS WHERE name = '" .$_POST["groupName"]. "'";
 	$result = mysqli_query($con, $query);
+	echo $query;
 	if(mysqli_num_rows($result) == 0)
 	{
 		$insert = mysqli_query($con, "INSERT INTO GROUPS (name, description, owner) VALUES ('".$_POST["groupName"]."', '".$_POST["groupDesc"]."', '".$_SESSION["username"]."')");
