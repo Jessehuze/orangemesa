251c59654187d309bb61cc42c7ec7716d3bf5e93
nrhvyc <nrhvyc.nickryanhughes@gmail.com>
2015-04-29 23:04:32 -0500

    Working on Settings

--- profile/groups.php
+++ profile/groups.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+  <link rel="stylesheet" type="text/css" href="settings.css">
 	<link rel="stylesheet" type="text/css" href="groups.css">
   
     <meta charset="utf-8">
