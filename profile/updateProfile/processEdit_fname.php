b4f240a1b97b12756f27308a3e2bab796f571023
nrhvyc <nrhvyc.nickryanhughes@gmail.com>
2015-04-29 22:39:33 -0500

    Working on Settings

--- profile/updateProfile/processEdit_fname.php
+++ profile/updateProfile/processEdit_fname.php
@@ -4,6 +4,7 @@
   $update = mysqli_query($con, "UPDATE PEOPLE 
                                 SET fname = '".$_POST["fname"]."' 
                                 WHERE username = '".$_SESSION["username"]."'"); 
+  echo $_POST["refer"];
   header("\"".$_POST["refer"]."\"");
   exit();
   
