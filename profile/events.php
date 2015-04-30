7a6d824de847371bdc579528850c633bb099ce38
jessehuze <jbhf39@mst.edu>
2015-04-29 20:02:25 -0500

    Switched back to old name display

--- profile/events.php
+++ profile/events.php
@@ -50,7 +50,10 @@
           <ul class="nav navbar-nav navbar-right">
             <li><a href="profile.php"><i class="fa fa-user"></i>
             <?php
-                echo $_SESSION["fname"];
+                require("/var/www/config.php");
+                $result = mysqli_query($con, "SELECT fname FROM PEOPLE WHERE username = '" .$_SESSION["User"]. "'");
+                $name = mysqli_fetch_array($result);
+                echo $name["fname"];
             ?>
             </a></li>
             <li><a href="friends.php">Friends</a></li>
