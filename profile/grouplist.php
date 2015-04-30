10c3263bc5709560710745a9d9131e512552ad51
Somsomguy <someguywhohasaname@gmail.com>
2015-04-29 21:34:52 -0500

    syntax error

--- profile/grouplist.php
+++ profile/grouplist.php
@@ -5,7 +5,7 @@
 	$query = "SELECT G.name, G.description, G.owner FROM GROUP_MEMBERS M, GROUPS G WHERE memberid = '" .$_SESSION["username"]. "' AND M.GID = G.groupid"; 
 	$result = mysqli_query($con, $query);
 	
-	while($row = mysqli_fetch_assoc($result)
+	while($row = mysqli_fetch_assoc($result))
 	{
 		echo $row["name"];
 		echo $row["description"];
