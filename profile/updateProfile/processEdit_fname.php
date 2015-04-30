348eaa192af0ed89c40e7dcf10176ac05d179148
nrhvyc <nrhvyc.nickryanhughes@gmail.com>
2015-04-29 19:18:24 -0500

    Working on Settings

--- profile/updateProfile/processEdit_fname.php
+++ profile/updateProfile/processEdit_fname.php
@@ -1,10 +1,10 @@
 <?php
   require("/var/www/config.php");
   session_start();
-    $insert = mysqli_query($con, "UPDATE PEOPLE
+    $update = mysqli_query($con, "UPDATE PEOPLE
                                   SET fname = '".$_POST["fname"]."'
                                   WHERE username = '".$_SESSION["username"]."' "); 
-  if ($insert)
+  if ($update)
   {
     $_SESSION["User"] = $_POST["username"];
   	header("Location: http://inceptisol.us.to:6670/profile/settings.php");
