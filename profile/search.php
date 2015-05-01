b2e5d3f0efc4c572037fe43b6a6823c5b8a3bfe7
nrhvyc <nrhvyc.nickryanhughes@gmail.com>
2015-04-30 22:20:57 -0500

    fixed settings on search page

--- profile/search.php
+++ profile/search.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <link rel="stylesheet" type="text/css" href="settings.css">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -31,6 +32,62 @@
   
   <body>
 
+    <!-- Settings Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">Settings</h4>
+          </div>
+          <div class="modal-body">
+           <div class="cont">
+           <div class = "demo">
+            <div class="editProfile">
+              <div class="editProfile_form">
+
+               <div class="editProfile_row">
+                <form action="updateProfile/processEdit_fname.php" method="POST">
+                <input name="refer" type="hidden" value="http://inceptisol.us.to:6670/profile/profile.php"/>
+                <input name="fname" type="text" class="edit__input fname" placeholder="First Name" />
+                <button name="update_fname" type="submit" class="edit__submit">Update First Name</button>
+                </form>
+               </div>
+
+               <div class="editProfile_row">
+                <form action="updateProfile/processEdit_minit.php" method="POST">
+                <input name="refer" type="hidden" value="http://inceptisol.us.to:6670/profile/profile.php"/>
+                <input name="minit" type="text" class="edit__input minit" placeholder="Middle Initial" />
+                <button name="update_minit" type="submit" class="edit__submit">Update Middle Initial</button>
+                </form>
+               </div>
+
+               <div class="editProfile_row">
+                <form action="updateProfile/processEdit_lname.php" method="POST">
+                <input name="refer" type="hidden" value="http://inceptisol.us.to:6670/profile/profile.php"/>
+                <input name="lname" type="text" class="edit__input lname" placeholder="Last Name" />
+                <button name="update_lname" type="submit" class="edit__submit">Update Last Name</button>
+                </form>
+               </div>
+
+               <div class="editProfile_row">
+                <form action="updateProfile/processEdit_description.php" method="POST">
+                <input name="refer" type="hidden" value="http://inceptisol.us.to:6670/profile/profile.php"/>
+                <textarea name="description" class="edit__input description" placeholder="Description" rows="6"></textarea>
+                <button name="update_description" type="submit" class="edit__submit">Update Description</button>
+                </form>
+               </div>
+
+              </div>
+             </div>
+           </div>
+           </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
     <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container-fluid">
         <div class="navbar-header">
@@ -58,7 +115,7 @@
             ?>
             </a></li>
             <li><a href="groups.php">Groups</a></li>
-            <li><a href="settings.php">Settings</a></li>
+            <li><a data-toggle="modal" data-target="#myModal" href="#myModal">Settings</a></li>
             <li><a href="../logout.php"><i class="fa fa-power-off"></i> Log Out</a></li>
           </ul>
           <form class="navbar-form navbar-left" action="search.php" method="GET">
