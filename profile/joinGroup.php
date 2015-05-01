fef00147545710dc5e679a8d9815c0dc3c5d511d
Somsomguy <someguywhohasaname@gmail.com>
2015-04-30 22:55:36 -0500

    fixes

--- profile/joinGroup.php
+++ profile/joinGroup.php
@@ -2,8 +2,18 @@
 	require("/var/config.php");
 	session_start();
 	
-	echo "join group";
-
+	$groupName = $_GET["group"];
+	$user = $_SESSION["username"];
+	
+	//Acquire group id from group name
+	$query = mysqli_query($con, "SELECT groupid FROM GROUPS WHERE name = '".$groupName."'"); 
+	$groupid = mysqli_fetch_array($query);
+	
+	//Insert user into group
+	$query = "INSERT INTO GROUP_MEMBERS (gid, memberid) VALUES ('".$groupid["groupid"]."', '".$user."')";
+	$insert = mysqli_query($con, $query);
+	
+	//header("Location: http://inceptisol.us.to::6670/profile/search.php");
 
 
 ?>
