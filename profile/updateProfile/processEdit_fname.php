b00581c2562567087dbc4e45238aba931c7e854c
jessehuze <jbhf39@mst.edu>
2015-04-30 21:16:55 -0500

    Added profile pages

--- profile/updateProfile/processEdit_fname.php
+++ profile/updateProfile/processEdit_fname.php
@@ -1,7 +1,7 @@
 <?php
   require("/var/config.php");
   session_start();
-  $username = mysqli_real_escape_string($con, $_POST["username"]);
+  $username = mysqli_real_escape_string($con, $_SESSION["username"]);
   $fname = mysqli_real_escape_string($con, $_POST["fname"]);
   $update = mysqli_query($con, "UPDATE PEOPLE 
                                 SET fname = '".$fname."' 
