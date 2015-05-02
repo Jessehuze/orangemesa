7829841e61de6f865f71b8de6da5e7567ab7446d
Jesse Hughes <jbhf39@mst.edu>
2015-05-02 16:24:32 -0500

    More testing

--- profile/profile.php
+++ profile/profile.php
@@ -490,12 +490,12 @@
                         else
                           $imageurl = "../images/user.png";
                         echo "<div class='row'>
-                          <div class='col-xs-4'>
+                          <div class='col-xs-3'>
                             <a href='profile.php?user=".$friend["username"]."'>
                               <img class='friendimg' src='".$imageurl."'/>
                              </a>
                           </div>
-                          <div class='col-xs-8'>
+                          <div class='col-xs-9'>
                             <a href='profile.php?user=".$friend["username"]."'>
                               <h5>".$friend["fname"]." ".$friend["lname"]." </h5>
                             </a>
