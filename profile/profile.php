2862505b10eccba4c11b013ab081842b871c46c5
Somsomguy <someguywhohasaname@gmail.com>
2015-05-01 23:15:00 -0500

    listing friends problems

--- profile/profile.php
+++ profile/profile.php
@@ -442,7 +442,7 @@
                 <div class="panel-body infobar">
                   <?php
 					
-					$query = "SELECT P.fname, P.lname, P.username FROM PEOPLE P, FRIENDS F WHERE P.username = F.friender AND P.username = '".$_SESSION["username"]."'";
+					$query = "SELECT P.fname, P.lname, P.username FROM PEOPLE P, FRIENDS F WHERE P.username = F.userid AND P.username = '".$_SESSION["username"]."'";
 					echo $query;
 					$friends_result = mysqli_query($con, $query); 
                     
