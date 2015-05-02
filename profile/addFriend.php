f9108af364f8bc79fd2958277fac9186c7a8bf45
Somsomguy <someguywhohasaname@gmail.com>
2015-05-01 23:20:21 -0500

    listing friends problems

--- profile/addFriend.php
+++ profile/addFriend.php
@@ -5,10 +5,10 @@
 	$friend = mysqli_real_escape_string($con, $_POST["user"]);
 	$friender = mysqli_real_escape_string($con, $_SESSION["username"]);
 	
-	$query = "INSERT INTO FRIENDS (userid, friendid, status) VALUES ('".$friender."', '".$friend."', a)";
+	$query = "INSERT INTO FRIENDS (userid, friendid, status) VALUES ('".$friender."', '".$friend."', 'a')";
 	echo $query;
 	$insert = mysqli_query($con, $query);
 	
-	header("Location: http://inceptisol.us.to:6670/profile/search.php");
+	//header("Location: http://inceptisol.us.to:6670/profile/search.php");
 
 ?>
