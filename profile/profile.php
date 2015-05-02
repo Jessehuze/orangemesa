d4d20c4afeaf131b1557675449b26840c5f109ad
Jesse Hughes <jbhf39@mst.edu>
2015-05-01 20:09:30 -0500

    No eastern culture

--- profile/profile.php
+++ profile/profile.php
@@ -410,7 +410,7 @@
         
         <style>
           .infobar{
-            max-height: calc(100vh);
+            max-height: calc(95vh);
             overflow-y: scroll;
             overflow-x: hidden; 
             word-wrap: break-word;
