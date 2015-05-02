57356eed57e5a351d100839b868e5d3aea328a70
Jesse Hughes <jbhf39@mst.edu>
2015-05-01 20:18:57 -0500

    This should do it

--- profile/profile.php
+++ profile/profile.php
@@ -410,7 +410,7 @@
         
         <style>
           .infobar{
-            max-height: calc(75vh);
+            max-height: calc(72vh);
             overflow-y: scroll;
             overflow-x: hidden; 
             word-wrap: break-word;
