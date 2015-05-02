0bfb9d5e9263e2e7060d5f5f4240c3d2db4e2a5b
Jesse Hughes <jbhf39@mst.edu>
2015-05-01 20:14:50 -0500

    The AI seems to fail around mountains

--- profile/profile.php
+++ profile/profile.php
@@ -410,7 +410,7 @@
         
         <style>
           .infobar{
-            max-height: calc(80vh);
+            max-height: calc(70vh);
             overflow-y: scroll;
             overflow-x: hidden; 
             word-wrap: break-word;
