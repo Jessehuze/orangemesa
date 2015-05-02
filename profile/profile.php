3ee2e4420070f5c2d8860e8358dfa9492e6fe637
Jesse Hughes <jbhf39@mst.edu>
2015-05-02 16:22:24 -0500

    that didn't work

--- profile/profile.php
+++ profile/profile.php
@@ -442,7 +442,6 @@
           }
           .friendimg{
               width: 100%;
-              height:100%
               border-radius: 512px;
               box-shadow: 0 0 10px rgba(0,0,0, .3)
             }
