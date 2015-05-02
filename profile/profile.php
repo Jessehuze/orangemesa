76e4a911bf02e2c9ccd91772a3f997d1a16f56cc
Jesse Hughes <jbhf39@mst.edu>
2015-05-01 20:06:40 -0500

    Testing viewport stuff

--- profile/profile.php
+++ profile/profile.php
@@ -410,7 +410,7 @@
         
         <style>
           .infobar{
-            max-height: calc(100vh - 175px);
+            max-height: calc(100vh);
             overflow-y: scroll;
             overflow-x: hidden; 
             word-wrap: break-word;
