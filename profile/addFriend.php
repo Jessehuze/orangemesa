406c4c0cfa6652704b81f2101468b3ce11341e57
Somsomguy <someguywhohasaname@gmail.com>
2015-05-02 00:14:58 -0500

    listing friends problems

--- profile/addFriend.php
+++ profile/addFriend.php
@@ -6,7 +6,7 @@
 	$friender = mysqli_real_escape_string($con, $_SESSION["username"]);
 	
 	$query = "INSERT INTO FRIENDS (userid, friendid, status) VALUES ('".$friender."', '".$friend."', 'a')";
-	
+	echo $query;
 	$insert = mysqli_query($con, $query);
 	if ($insert == false)
 	{
