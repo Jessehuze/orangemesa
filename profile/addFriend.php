28450955c1c2cb704322977ea75fcfb5c4b4d182
Jesse Hughes <jbhf39@mst.edu>
2015-05-02 10:40:08 -0500

    Added check for if already friends

--- profile/addFriend.php
+++ profile/addFriend.php
@@ -5,19 +5,23 @@
 	$friend = mysqli_real_escape_string($con, $_POST["user"]);
 	$friender = mysqli_real_escape_string($con, $_SESSION["username"]);
   
-	$query = "INSERT INTO FRIENDS (userid, friendid, request_state) VALUES ('".$friender."', '".$friend."', 'a')";
-	echo $query;
-	$insert = mysqli_query($con, $query);
-	if ($insert == false)
-	{
-		echo "failure";
-	}
-	else
-	{
-		echo "success";
-	}
-	echo $insert;
+  $friendstatus = mysqli_query($con, "SELECT * FROM FRIENDS
+                                                  WHERE (userid = '".$friend."' AND friendid = '".$friender."') OR 
+                                                            (userid = '".$friend."' AND friendid = '".$friender."') ");
+  
+  if (mysqli_num_rows($friendstatus) == 0)
+  {
+    $query = "INSERT INTO FRIENDS (userid, friendid, request_state) VALUES ('".$friender."', '".$friend."', 'a')";
+    echo $query;
+    $insert = mysqli_query($con, $query);
+    header("Location: http://inceptisol.us.to:6670/profile/profile.php");
+    exit();
+  }
+  else
+  {
+    header("Location: http://inceptisol.us.to:6670/profile/profile.php");
+    exit();
+  }
 	
-	//header("Location: http://inceptisol.us.to:6670/profile/profile.php");
 
 ?>
