752db3e1882f76e9fbf2aca5466843f932d8fc76
Somsomguy <someguywhohasaname@gmail.com>
2015-05-02 19:05:04 -0500

    listing groups on profile page

--- profile/profile.php
+++ profile/profile.php
@@ -576,8 +576,31 @@
               </div>
               <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
                 <div class="panel-body infobar">
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                </div>
+                 <?php
+					
+					$query = "SELECT gpid, name, description FROM GROUPS WHERE groupid IN
+							(SELECT gid FROM GROUP_MEMBERS WHERE memberid = '".$_SESSION["username"]."')";
+					
+					$group_result = mysqli_query($con, $query);
+					
+					if(mysqli_num_rows($group_result != 0)
+					{
+						while ($group = mysqli_fetch_array($group_result))
+						{
+							//Picture for group?
+							
+							echo "<div class='row'>
+								<h2>".$group["name"]."</h2>
+								</div>
+								<div class='row'>
+								<h5>".$group["description"]."</h5>
+								<br>
+								</div>";
+							
+						}
+					}
+				 ?>
+				 </div>
               </div>
             </div>
             <div class="panel panel-default">
