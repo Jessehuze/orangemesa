211be0160dc1b814811cd50f445d30bbe19c7807
Somsomguy <someguywhohasaname@gmail.com>
2015-05-03 15:36:44 -0500

    adding comments

--- profile/createGroup.php
+++ profile/createGroup.php
@@ -1,16 +1,24 @@
 <?php
+	//Database connection code
 	require("/var/config.php");
 	session_start();
-  $groupName = mysqli_real_escape_string($con, $_POST["groupName"]);
-  $groupDesc = mysqli_real_escape_string($con, $_POST["groupDesc"]);
+	
+	//Sanitize the information passed to protect against sql injection
+    $groupName = mysqli_real_escape_string($con, $_POST["groupName"]);
+    $groupDesc = mysqli_real_escape_string($con, $_POST["groupDesc"]);
+	
+	//Check to see if group name is already taken
 	$query = "SELECT name FROM GROUPS WHERE name = '" .$groupName. "'";
 	$result = mysqli_query($con, $query);
+	
+	//If name isn't taken
 	if(mysqli_num_rows($result) == 0)
 	{
 		//Create Group in database
 		$query = "INSERT INTO GROUPS (name, description, owner) VALUES ('".$groupName."', '".$groupDesc."', '".$_SESSION["username"]."')";
 		$insert = mysqli_query($con, $query);
 		
+		//Retrieve new group id
 		$query = mysqli_query($con, "SELECT groupid, owner FROM GROUPS WHERE name = '".$groupName."'");
 		$row1 = mysqli_fetch_array($query);
 		
@@ -20,10 +28,12 @@
 	}
 	else
 	{
-		header("Location: http://inceptisol.us.to:6670/profile/groups.php?error=groupnametaken");
+		//Redirect if group name is taken
+		header("Location: http://inceptisol.us.to:6670/profile/profile.php?error=groupnametaken");
 		exit();
 	}
-	//header("Location: http://inceptisol.us.to:6670/profile/groups.php");
+	//Redirect to profile page
+	//header("Location: http://inceptisol.us.to:6670/profile/profile.php");
 	exit();
 
 ?>
