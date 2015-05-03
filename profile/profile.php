2a9a2982406f336f5fb6b1011a137424f09d584b
jessehuze <jbhf39@mst.edu>
2015-05-03 18:21:48 -0500

    Added box shadows

--- profile/profile.php
+++ profile/profile.php
@@ -531,7 +531,9 @@
               height: 50px;
             }
             .usrpost{
-              box-shadow: 0 0 5px rgba(0,0,0, 0.5);
+              padding: 20px;
+              margin: 10px 0;
+              box-shadow: 0 0 7px rgba(0,0,0, 0.5);
             }
         </style>
         
