2c12da77de3eb5cb0b0c645e8c1c581b31c413e2
Somsomguy <someguywhohasaname@gmail.com>
2015-05-03 15:50:45 -0500

    Some more comments

--- login/processlogin.php
+++ login/processlogin.php
@@ -1,18 +1,29 @@
 <?php
+
+  //Database connection code
   require("/var/config.php");
   session_start();
+  
   if($_POST["username"] != "" && $_POST["pass"] != "" && isset($_POST["login"])) {
-    $username = mysqli_real_escape_string($con, $_POST["username"]);
+    
+	//Sanitize data input to prevent sql injection
+	$username = mysqli_real_escape_string($con, $_POST["username"]);
     $usr_pass = mysqli_real_escape_string($con, $_POST["pass"]);
-    $result = mysqli_query($con, "SELECT username, fname, usr_pass FROM PEOPLE WHERE username = '" .$username. "'");
+    
+	//Retrieve password from given username
+	$result = mysqli_query($con, "SELECT username, fname, usr_pass FROM PEOPLE WHERE username = '" .$username. "'");
     $row = mysqli_fetch_array($result);
     
+	//Check to see if password and first names match
     if (password_verify($_POST["pass"], $row["usr_pass"])) {
       $_SESSION["username"] = $row["username"];
       $_SESSION["fname"] = $row["fname"];
-      header("Location: http://inceptisol.us.to:6670/profile/profile.php");
+      
+	  //Redirect to profile page
+	  header("Location: http://inceptisol.us.to:6670/profile/profile.php");
       exit();
     }
+	//If the password and first name don't match redirect to login page
     header("Location: http://inceptisol.us.to:6670/login/login.php?error=Incorrect Username/Password.");
     exit();
   }
--- profile/joinEvent.php
+++ profile/joinEvent.php
@@ -1,7 +1,9 @@
 <?php
+	//Database connection code
 	require("/var/config.php");
 	session_start();
 	
+	//Sanitize data input to prevent sql injection
 	$eventid = mysqli_real_escape_string($con, $_POST["event"]);
 	$user = mysqli_real_escape_string($con, $_SESSION["username"]);
 	
@@ -9,6 +11,7 @@
 	$query = "INSERT INTO EVENT_INVITES VALUES ('".$eventid."', '".$user."', 'g')";
 	$insert = mysqli_query($con, $query);
 	
+	//Redirect to profile page
 	header("Location: http://inceptisol.us.to::6670/profile/search.php");
 
 
