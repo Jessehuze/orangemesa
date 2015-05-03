c3f307648b124647bb6310959d49477f0bbc8acd
Somsomguy <someguywhohasaname@gmail.com>
2015-05-02 22:51:51 -0500

    aggegrate count

--- profile/profile.php
+++ profile/profile.php
@@ -531,14 +531,14 @@
                         $result = mysqli_query($con, $query); 
                         $name = mysqli_fetch_array($result);
 						
-						$query = "SELECT userid, COUNT(distinct userid) FROM FRIENDS WHERE userid='".$user."'";
+						$query = "SELECT userid, COUNT(DISTINCT(userid)) FROM FRIENDS WHERE userid='".$user."'";
 						$result = mysqli_query($con, $query);
 						$count = mysqli_fetch_array($result);
 						
                         if ($user != $_SESSION["username"])
-                          echo $name["fname"]." Follows ".$count["COUNT(distinct userid)"]." people:";
+                          echo $name["fname"]." Follows ".$count["COUNT(DISTINCT(userid))"]." people:";
                         else
-                          echo "You follow " .$count["count(distinct userid)"]." people:";
+                          echo "You follow " .$count["count(DISTINCT(userid))"]." people:";
                       ?>
                   </a>
                 </h4>
