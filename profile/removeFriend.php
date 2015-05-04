e9f72e4382c6a8f9c6fe0aef0aa86160d013fa8e
Somsomguy <someguywhohasaname@gmail.com>
2015-05-03 21:55:32 -0500

    unfollow button code

---
+++ profile/removeFriend.php
@@ -0,0 +1,15 @@
+<?php
+  require("/var/config.php");
+  session_start();
+  
+  $friend = mysqli_real_escape_string($con, $_POST["friend"]);
+  $user = mysqli_real_escape_string($con, $_SESSION["username"]);
+  
+  $query = "DELETE FROM FRIENDS WHERE userid = '".$user."' AND friendid = '".$friend."'";
+  $delete = mysqli_query($con, $query);
+
+  //Redirect to profile page
+  header("Location: http://inceptisol.us.to::6670/profile/profile.php");
+  exit();
+
+?>
