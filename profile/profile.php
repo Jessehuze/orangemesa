d5f6cd9ac7a1580125c2a11fc0d6140bf8a85ba7
jessehuze <jbhf39@mst.edu>
2015-05-03 19:10:44 -0500

    Added nice pointer to a pointer to a pointer

--- profile/profile.php
+++ profile/profile.php
@@ -381,6 +381,7 @@
         top: -40px;
         left: 188px;
         margin-bottom: -40px;
+        cursor: pointer;
       }
     </style>
     <div class="container-fluid">
@@ -388,7 +389,7 @@
         <div class="col-sm-3 col-md-2 sidebar userinfo profileSideBar">
           <div>
             <div class="usrimg"></div>
-            <div class="updateimg" id="updateimg"><i class="fa fa-gear"></i></div>
+            <div data-toggle="modal" data-target="#propic" class="updateimg" id="updateimg"><i class="fa fa-gear"></i></div>
           </div>
           <hr>
           <h2>
