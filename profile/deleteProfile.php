c3f8b4def68070e1893820f96b0e1be499897a50
jessehuze <jbhf39@mst.edu>
2015-05-04 16:03:40 -0500

    Ok, found the bug

--- profile/deleteProfile.php
+++ profile/deleteProfile.php
@@ -4,7 +4,7 @@
   
   if ($_POST["delete"] == "true")
   {
-    $query = "DELETE FROM PEOPLE WHERE username = ".$_SESSION["user"];
+    $query = "DELETE FROM PEOPLE WHERE username = '".$_SESSION["user"]."'";
     $delete = mysqli_query($con, $query);
     session_destroy();
     header("Location: http://inceptisol.us.to:6670/login/login.php");
