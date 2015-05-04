2ad5ad0fdb03d76703eed4d67c0b99595bdfe74c
Somsomguy <someguywhohasaname@gmail.com>
2015-05-03 21:48:45 -0500

    leave group php code

--- profile/leavegroup.php
+++ profile/leavegroup.php
@@ -2,10 +2,10 @@
   require("/var/config.php");
   session_start();
   
-  $event = mysqli_real_escape_string($con, $_POST["event"]);
+  $group = mysqli_real_escape_string($con, $_POST["group"]);
   $user = mysqli_real_escape_string($con, $_SESSION["username"]);
   
-  $query = "DELETE FROM EVENT_INVITES WHERE invitee = '".$user."' AND eventid = '".$event."'";
+  $query = "DELETE FROM GROUP_MEMBERS WHERE gid = '".$group."' AND memberid = '".$user."'";
   $delete = mysqli_query($con, $query);
 
   //Redirect to profile page
