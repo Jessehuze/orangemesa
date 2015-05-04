fee1aaf61bc3b6aa7d553f399205cbc6031a31b5
Somsomguy <someguywhohasaname@gmail.com>
2015-05-03 20:16:47 -0500

    Working on unfollow button

--- profile/profile.php
+++ profile/profile.php
@@ -691,14 +691,16 @@
                              </a>
                           </div>
                           <div class='col-xs-9'>
-                            <a href='profile.php?user=".$friend["username"]."'>
+                            <div class='col-xs-9'>
+							<a href='profile.php?user=".$friend["username"]."'>
                               <h5>".$friend["fname"]." ".$friend["lname"]." </h5>
                             </a>
-							
+							</div>
+							<div class='col-xs-3'>
 							<form action='removeFriend.php' method='POST'>
 					          <button class='btn addbtn btn-default' name='friend' value='".$friend["username"]."' type='submit'>Unfollow</button>
                             </form>
-							
+							</div>
                           </div>
                         </div>";
                       }
