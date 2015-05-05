f8359b04f617abd6ae45660c35dfc69f87408b8d
Somsomguy <someguywhohasaname@gmail.com>
2015-05-04 20:41:58 -0500

    moar comments

--- profile/leavegroup.php
+++ profile/leavegroup.php
@@ -1,10 +1,13 @@
 <?php
+  //Code to connect to database
   require("/var/config.php");
   session_start();
   
+  //Sanitize database input to prevent sqli injection
   $group = mysqli_real_escape_string($con, $_POST["group"]);
   $user = mysqli_real_escape_string($con, $_SESSION["username"]);
   
+  //Delete group member entry from database
   $query = "DELETE FROM GROUP_MEMBERS WHERE gid = '".$group."' AND memberid = '".$user."'";
   $delete = mysqli_query($con, $query);
 
