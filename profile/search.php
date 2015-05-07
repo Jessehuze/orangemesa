4db05ca500496cfdfaeb78436fc7c7d630287b0f
jessehuze <jbhf39@mst.edu>
2015-05-06 19:25:06 -0500

    wtf

--- profile/search.php
+++ profile/search.php
@@ -42,7 +42,7 @@
 
 
   <body>
-
+    <?php echo "wtf" ?>
     <!-- Settings Modal -->
     <div class="modal fade" id="settingsModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
       <div class="modal-dialog">
