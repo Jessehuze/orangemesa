d8182ff240fdad087a39a5371b660aa1552d7e51
jessehuze <jbhf39@mst.edu>
2015-05-06 20:02:53 -0500

    fixing bugs

--- profile/profile.php
+++ profile/profile.php
@@ -613,7 +613,7 @@
                     <div class='col-md-2'>";
                       if ($msg["sender"] == $_SESSION["username"])
                         echo "<a  href='deletepost.php?user=".$user."&postid=".$msg["postid"]."' class='deletepost'><i class='fa fa-times'></i></a>";
-                    echo "<p>"$sentTime."</p>
+                    echo "<p>".$sentTime."</p>
                     </div>
                   </div>";
                 }
@@ -623,7 +623,7 @@
                     <div class='col-sm-2'>";
                     if ($user == $_SESSION["username"] || $msg["sender"] == $_SESSION["username"])
                       echo "<a href='deletepost.php?user=".$user."&postid=".$msg["postid"]."' class='deletepost pull-right'><i class='fa fa-times'></i></a>";
-                    echo "<p>"$sentTime."</p>
+                    echo "<p>".$sentTime."</p>
                     </div>
                     <div style='text-align: right;' class='col-sm-8 usrtxt'>
                       <a href='profile.php?user=".$msg["sender"]."'>
