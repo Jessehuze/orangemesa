15fd0321fcf45c602827e24ef3b057f6542f69f1
jessehuze <jbhf39@mst.edu>
2015-05-06 22:26:15 -0500

    asdlfsd?

--- profile/profile.php
+++ profile/profile.php
@@ -339,7 +339,13 @@
                 <div class="createGroupDate__row">
                   <div class='col-sm-6'>
                   <input type='text' class="form-control" id='datetimepicker4' />
-                  <input name="refer" type="hidden" value="http://inceptisol.us.to:6670/profile/profile.php"/>
+                  </div>
+                  <script type="text/javascript">
+                      $(function () {
+                          $('#datetimepicker4').datetimepicker();
+                      });
+                  </script>
+                <input name="refer" type="hidden" value="http://inceptisol.us.to:6670/profile/profile.php"/>
                 <button name="createGroup" type="submit" class="edit__submit">Create Event</button>
               </form>
 
